3ff51a481b1724c6f347d6124130867130c999fc
Himanshu Singh Banoula <himbravo@gmail.com>
2020-03-15 13:11:52 +0530

    changes pushed
    changes pushed

---
+++ MainClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlinClock
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            TimeFormatCreator factory = new ConcereteTimeFormatCreator();
+
+            try
+            {
+                Console.WriteLine("Input Format Type: 1. Simple(hh:mm:ss) ");
+                String inputTimeFormat = Console.ReadLine();
+                ITimeFormat inputFormat = factory.SetTimeFormat(inputTimeFormat);
+                try
+                {
+                    Console.WriteLine("Enter Time:");
+                    String t = Console.ReadLine();
+                    Time inputTime = inputFormat.SetInput(t);
+                    try
+                    {
+                        Console.WriteLine("Output Format Type:\n 1. Default \n2. BerlinClock");
+                        String outputTimeFormat = Console.ReadLine();
+                        ITimeFormat outputFormat = factory.GetTimeFormat(outputTimeFormat);
+                        Console.WriteLine("\nOutput Time:");
+                        Console.WriteLine(outputFormat.GetOutput(inputTime));
+                    }
+                    catch (Exception eee)
+                    {
+                        Console.WriteLine(eee.Message);
+                    }
+                }
+                catch (Exception ee)
+                {
+                    Console.WriteLine(ee.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
+
