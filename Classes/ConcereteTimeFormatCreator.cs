f29ef583289f2bb57e1529f486f6eb9cb16bfe4a
Himanshu Singh Banoula <himbravo@gmail.com>
2020-03-15 13:14:40 +0530

    commit

---
+++ Classes/ConcereteTimeFormatCreator.cs
@@ -0,0 +1,26 @@
+﻿namespace BerlinClock
+{
+    public class ConcereteTimeFormatCreator : TimeFormatCreator
+    {
+        //Dependency Injection for Setting the input Time Format
+        public override ITimeFormat SetTimeFormat(string format)
+        {
+            switch (format)
+            {
+                case "1": return new SimpleFormat();
+                default: return new DefaultFormat();
+            }
+        }
+
+        //Dependency Injection for Setting the output Time Format
+        public override ITimeFormat GetTimeFormat(string format)
+        {
+            switch (format)
+            {
+                case "1": return new SimpleFormat();
+                case "2": return new BerlinClockFormat();
+                default: return new DefaultFormat();
+            }
+        }
+    }
+}
---
+++ Classes/TimeFormatCreator.cs
@@ -0,0 +1,11 @@
+﻿namespace BerlinClock
+{
+    public abstract class TimeFormatCreator
+    {
+        //Gets the Input Time Format
+        public abstract ITimeFormat SetTimeFormat(string format);
+
+        //Gets the Output Time Format
+        public abstract ITimeFormat GetTimeFormat(string format);
+    }
+}
